06f3389089d3416d76ac4f86d6857d0db09d9c7d
kuzmovych <kuzmovich.box@mail.ru>
2017-03-12 15:38:06 +0100

    FIX links to errors in files tex folder

--- Bachelor's thesis/compile.sh
+++ Bachelor's thesis/compile.sh
@@ -3,21 +3,22 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-TEX_FILE_NAME=$(echo *.tex)
-PDF_FILE_NAME=pdf/${TEX_FILE_NAME/tex/pdf}
-LOG_FILE_NAME=pdf/compilation.log
+OUT_DIR=pdf
+TEX_FILE=$(echo *.tex)
+PDF_FILE=${OUT_DIR}/${TEX_FILE/tex/pdf}
+LOG_FILE=${OUT_DIR}/compilation.log
 
-pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=pdf \
-    ${TEX_FILE_NAME} > ${LOG_FILE_NAME}
+mkdir -p ${OUT_DIR}
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -output-directory=${OUT_DIR} \
+    ${TEX_FILE} > ${LOG_FILE}
 
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}"
-    echo -n "Successfully Compiled ${PDF_FILE_NAME}"
-    xdg-open ${PDF_FILE_NAME}
+    echo -n "Successfully Compiled ${PDF_FILE}"
+    xdg-open ${PDF_FILE}
 else
-    sed "s/^\.\/\(${TEX_FILE_NAME}:[0-9]*\):/\.(\1)/" ${LOG_FILE_NAME}
-    # grep ${tex_filename} ${LOG_FILE_NAME} | sed "s/^\.\/\(${tex_filename}:[0-9]*\):/\.(\1)/"
+    head -n -3 ${LOG_FILE} | sed "s/^\..*\/\([^\/]*\.tex:[0-9]*\):/\.(\1)/"
     echo -e "${RED}"
-    echo -n "Compilation Error ${TEX_FILE_NAME}"
+    echo -n "Compilation Error ${TEX_FILE}"
 fi
 echo -e "${NC}"
